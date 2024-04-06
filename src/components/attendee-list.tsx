251f77b7a3bba7438e054c0de2c2bf5f1a75a1ed
devgoncalo <goncalopintoneto@gmail.com>
2024-04-06 12:38:48 +0100

    feat: set current page with url state

--- src/components/attendee-list.tsx
+++ src/components/attendee-list.tsx
@@ -32,7 +32,15 @@ interface Attendee {
 
 export function AttendeeList() {
   const [search, setSearch] = useState("");
-  const [page, setPage] = useState(1);
+  const [page, setPage] = useState(() => {
+    const url = new URL(window.location.toString());
+
+    if (url.searchParams.has("page")) {
+      return Number(url.searchParams.get("page"));
+    }
+
+    return 1;
+  });
 
   const [total, setTotal] = useState(0);
   const [attendees, setAttendees] = useState<Attendee[]>([]);
@@ -58,25 +66,35 @@ export function AttendeeList() {
       });
   }, [page, search]);
 
+  function setCurrentPage(page: number) {
+    const url = new URL(window.location.toString());
+
+    url.searchParams.set("page", String(page));
+
+    window.history.pushState({}, "", url);
+
+    setPage(page);
+  }
+
   function onSearchInputChange(event: React.ChangeEvent<HTMLInputElement>) {
     setSearch(event.target.value);
-    setPage(1);
+    setCurrentPage(1);
   }
 
   function goToFirstPage() {
-    setPage(1);
-  }
-
-  function goToNextPage() {
-    setPage(page + 1);
-  }
-
-  function goToPreviousPage() {
-    setPage(page - 1);
+    setCurrentPage(1);
   }
 
   function goToLastPage() {
-    setPage(totalPages);
+    setCurrentPage(totalPages);
+  }
+
+  function goToPreviousPage() {
+    setCurrentPage(page - 1);
+  }
+
+  function goToNextPage() {
+    setCurrentPage(page + 1);
   }
 
   return (
